914dcbe35b7ab50f12497e83895bc57374131bf4
Alex Marquez <marquez798@gmail.com>
2023-07-17 21:26:22 -0500

    finished post delete and put router

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,14 +31,43 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, { where: {id: req.params.id} });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      return(res.status(404).json({ message: 'No category found with that id!' }));
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
